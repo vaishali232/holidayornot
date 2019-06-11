0e7fa2c030033dca865ccdf3285aebb4b373fe59
vaishali232 <51689354+vaishali232@users.noreply.github.com>
2019-06-11 20:19:00 +0530

    Create holiday.c

---
+++ holiday.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    char s[1000];
+    scanf("%s",s);
+    if(s=="Sunday"||s=="Saturday")
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+       
+    
+    
+}

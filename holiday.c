023e474859e3a6b15e3585645a0e0799be415a6b
vaishali232 <51689354+vaishali232@users.noreply.github.com>
2019-06-11 20:27:13 +0530

    Update holiday.c

--- holiday.c
+++ holiday.c
@@ -4,15 +4,30 @@ int main()
 {
     
     char s[1000];
-    scanf("%s",s);
-    if(s=="Sunday"||s=="Saturday")
+    int n;
+    scanf("%s%n",s,&n);
+    for(int i=0;i<i+2;i++)
     {
-        printf("yes");
+        if(s[i]=='S')
+        {
+            if(s[i+1]=='u')
+            {
+                printf("yes");
+                break;
+            }
+            else if(s[i+1]=='a')
+            {
+                printf("yes");
+                break;
+            }
+        }
+        else
+        {
+            printf("no");
+            break;
+        }
     }
-    else
-    {
-        printf("no");
-    }
+    
        
     
     
